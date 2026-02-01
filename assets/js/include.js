a70b9075bb66840018ac126b2aad0679e09697f0
chanholee87 <chanho.lee87@gmail.com>
2026-02-01 22:37:01 +0900

    .

---
+++ assets/js/include.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const targets = document.querySelectorAll('[data-include]');
+    let count = 0;
+
+    targets.forEach(el => {
+        fetch(el.dataset.include)
+            .then(res => res.text())
+            .then(html => {
+                el.innerHTML = html;
+                count++;
+
+                if (count === targets.length) {
+                    document.dispatchEvent(new Event('include:done'));
+                }
+            });
+    });
+});
